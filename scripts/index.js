9fe32afe98b9190959102b851055cb81df786897
LV <viplong12hh@gmail.com>
2024-08-16 09:59:41 -0500

    adjust function

--- scripts/index.js
+++ scripts/index.js
@@ -28,7 +28,7 @@ const initialCards = [
 const editProfileButton = document.querySelector(".profile__edit-button");
 const editPopup = document.querySelector(".modal");
 const editPopupCloseButton = document.querySelector(".modal__close-button");
-const editPopupSaveButton = document.querySelector(".modal__save-button");
+const editPopupForm = document.forms["edit-profile-form"];
 const profileName = document.querySelector(".profile__name");
 const profileBio = document.querySelector(".profile__bio");
 const inputName = document.querySelector("#profile-name-input");
@@ -36,26 +36,6 @@ const inputBio = document.querySelector("#profile-bio-input");
 const cardListEl = document.querySelector(".gallery__list");
 const cardTemplate =
   document.querySelector("#card-template").content.firstElementChild;
-
-editProfileButton.addEventListener("click", function () {
-  inputName.value = profileName.textContent;
-  inputBio.value = profileBio.textContent;
-  editPopup.classList.remove("modal_close");
-  editPopup.classList.add("modal_open");
-});
-
-editPopupCloseButton.addEventListener("click", function () {
-  editPopup.classList.remove("modal_open");
-  editPopup.classList.add("modal_close");
-});
-
-editPopupSaveButton.addEventListener("click", function (event) {
-  event.preventDefault();
-  profileName.textContent = inputName.value;
-  profileBio.textContent = inputBio.value;
-  editPopup.classList.remove("modal_open");
-  editPopup.classList.add("modal_close");
-});
 
 function getCardElement(data) {
   const cardElement = cardTemplate.cloneNode(true);
@@ -66,6 +46,29 @@ function getCardElement(data) {
   cardTitle.textContent = data.name;
   return cardElement;
 }
+
+function closeProfileModal() {
+  if (editPopup.classList.contains("modal_open")) {
+    editPopup.classList.remove("modal_open");
+  } else {
+    editPopup.classList.add("modal_open");
+  }
+}
+
+editProfileButton.addEventListener("click", function () {
+  inputName.value = profileName.textContent;
+  inputBio.value = profileBio.textContent;
+  closeProfileModal();
+});
+
+editPopupCloseButton.addEventListener("click", closeProfileModal);
+
+editPopupForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+  profileName.textContent = inputName.value;
+  profileBio.textContent = inputBio.value;
+  closeProfileModal();
+});
 
 initialCards.forEach((data) => {
   const cardElement = getCardElement(data);
